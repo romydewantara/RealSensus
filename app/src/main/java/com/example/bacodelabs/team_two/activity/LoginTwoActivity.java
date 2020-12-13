6b47eff9a814d2319b32949f8c0f1cd8a3dbb266
aditianugraha3 <nugrahaaditia39@gmail.com>
2020-12-13 16:23:01 +0700

    Add login activty two bud

---
+++ app/src/main/java/com/example/bacodelabs/team_two/activity/LoginTwoActivity.java
@@ -0,0 +1,16 @@
+package com.example.bacodelabs.team_two.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bacodelabs.R;
+
+public class LoginTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_togin_two);
+    }
+}
