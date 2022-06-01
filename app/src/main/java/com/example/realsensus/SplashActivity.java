9820384aa79e61523685887cfbf037cf6aec979a
Dewantara Romy <dewantararomy@gmail.com>
2022-06-01 17:54:44 +0700

    Test Learn App - add new activity - add new font styles - add new icon

---
+++ app/src/main/java/com/example/realsensus/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.realsensus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
