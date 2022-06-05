47a37c706ae3fe2f234291dc2a6986ce98c6c3e5
Dewantara Romy <dewantararomy@gmail.com>
2022-06-06 06:50:58 +0700

    delete plugin from gradle module fix downgrading gradle version

--- app/src/main/java/com/example/realsensus/SplashActivity.java
+++ app/src/main/java/com/example/realsensus/SplashActivity.java
@@ -3,7 +3,9 @@ package com.example.realsensus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent openMainActivity = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(openMainActivity);
+            finish();
+        }, 5000);
     }
 }
