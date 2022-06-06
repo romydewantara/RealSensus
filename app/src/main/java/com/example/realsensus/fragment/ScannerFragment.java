2d021cf5e9959ef1470901508df596bf2c82ad22
Dewantara Romy <dewantararomy@gmail.com>
2022-06-06 09:54:57 +0700

    Initiate the required fragments.

---
+++ app/src/main/java/com/example/realsensus/fragment/ScannerFragment.java
@@ -0,0 +1,51 @@
+package com.example.realsensus.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.realsensus.R;
+
+/**
+ * Created by Muhammad Fakhri Pratama
+ * github: ari.japindo@gmail.com | akufakhri61
+ */
+public class ScannerFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    public ScannerFragment() {
+        // Required empty public constructor
+    }
+
+    public static ScannerFragment newInstance(String param1, String param2) {
+        ScannerFragment fragment = new ScannerFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_scanner, container, false);
+    }
+}
